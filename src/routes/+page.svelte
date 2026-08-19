1131bf276c279ae2997d574b9e997fb9d7a2b697
Elijah Flader <elijahflader@gmail.com>
2026-08-19 16:26:07 -0400

    Add return anyway

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,12 @@
 </script>
 
 {#if from}
-  <p>You were redirected from: <a href={from}>{from}</a></p>
+  <p>You were redirected from: {from}</p>
 {/if}
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+{#if from}
+    <p> To go back click this link: <a href={from}>{from + "/?snooze=10"}</a></p>
+{/if}
