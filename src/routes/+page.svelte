7f2a633ea3548a52257ebc0fcfc4893d141b3faa
Elijah Flader <elijahflader@gmail.com>
2026-08-19 15:45:25 -0400

    Intitial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  let from = new URLSearchParams(window.location.search).get('from');
+</script>
+
+{#if from}
+  <p>You were redirected from: <a href={from}>{from}</a></p>
+{/if}
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
